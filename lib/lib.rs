1f09391c36998b1b8a0f3c337f73418be404604f
Alberto Corona <ac@albertocorona.com>
2015-10-10 18:25:50 -0500

    lib: update builder functions, add temp split
    	- PkgInfo will be used to store matadata within the package
    	- Builder's new fn's serve to create a package tarball.
    	  Currently setting environment variables is not working
    	  correctly and the current process is not inheriting them.
    
    Signed-off-by: Alberto Corona <ac@albertocorona.com>

--- lib/lib.rs
+++ lib/lib.rs
@@ -5,16 +5,37 @@ extern crate tar;
 
 pub static MPM: Prog = Prog { name: "mpm", vers: "0.1.0", yr: "2015", };
 
-use rpf::*;
-use tar::Archive;
+use std::fs;
+use std::env;
+use std::io;
+use std::process::Command;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io;
+
+use rpf::*;
+use tar::Archive;
 use toml::{Value};
-use rustc_serialize::Encoder;
-use rustc_serialize::Encodable;
+use rustc_serialize::{Encoder,Encodable};
 use rustc_serialize::json::Json;
 use rustc_serialize::json;
+
+// This trait is temporary until `split` is considered stable in std
+pub trait Splits<T> {
+    fn split_frst(&self) -> Option<(&T, &[T])>;
+    fn split_lst(&self) -> Option<(&T, &[T])>;
+}
+
+impl<T> Splits<T> for [T] {
+    fn split_frst(&self) -> Option<(&T, &[T])> {
+        if self.is_empty() { None } else { Some((&self[0], &self[1..]))}
+    }
+    fn split_lst(&self) -> Option<(&T, &[T])> {
+        let len = self.len();
+        if self.is_empty() { None } else {
+            Some((&self[len - 1], &self[..(len -1)]))
+        }
+    }
+}
 
 #[derive(RustcDecodable,RustcEncodable,Debug,Default,PartialEq)]
 pub struct BuildFile {
@@ -22,7 +43,7 @@ pub struct BuildFile {
     vers: Option<String>,
     build: Option<Vec<String>>,
     desc: Option<String>,
-    dest: Option<String>,
+    prefix: Option<String>,
     install: Option<Vec<String>>,
     makedeps: Option<Vec<String>>,
     deps: Option<Vec<String>>,
@@ -32,34 +53,44 @@ pub struct BuildFile {
     license: Option<String>,
     provides: Option<String>,
     conflicts: Option<Vec<String>>,
+    maintainers: Option<Vec<String>>,
+}
+
+#[derive(RustcDecodable,RustcEncodable,Debug,Default,PartialEq)]
+pub struct PkgInfo {
+    name: String,
+    vers: String,
+    builddate: String,
+    url: String,
+    size: u64,
+    arch: String,
+    license: String,
+    conflicts: Vec<String>,
+    provides: String,
 }
 
 impl BuildFile {
     pub fn new() -> BuildFile {
-        let pkg: BuildFile = Default::default();
-        return pkg;
+        Default::default()
     }
 
-    pub fn from_file(file: &str) -> BuildFile {
-        let pkg: BuildFile = match json::decode(&parse_pkg_file(file).to_string()) {
-            Ok(k) => { k },
-            Err(e) => {
-                MPM.error(e.to_string(), ExitStatus::Error);
-                panic!();
-            }
-        };
-        return pkg;
+    pub fn from_file(file: &str) -> Result<BuildFile, json::DecoderError> {
+        match json::decode(&parse_pkg_file(file).to_string()) {
+            Ok(k) => { return Ok(k) },
+            Err(e) => { return Err(e) }
+        }
     }
 
     pub fn print_json(&self) {
         println!("{}", json::as_pretty_json(&self));
     }
-
 }
 
 pub trait Builder {
     fn create_pkg(&self);
     fn create_tar_file(&self) -> Result<File, io::Error>;
+    fn set_env(&self);
+    fn build(&self);
 }
 
 impl Builder for BuildFile {
@@ -81,6 +112,60 @@ impl Builder for BuildFile {
             }
         };
         let archive = Archive::new(tar);
+        let dir_iter = match fs::read_dir("build") {
+            Ok(s) => { s },
+            Err(e) => { panic!(e); }
+        };
+        for entry in dir_iter {
+            let entry = entry.unwrap();
+            archive.append_path(entry.path());
+        }
+        match archive.finish() {
+            Ok(_) => {
+                println!("{}: package {} successfully built",
+                         MPM.name.bold(),
+                         &self.name.clone().unwrap().bold());
+            },
+            Err(e) => {
+                println!("{}: package {} could not be build",
+                         MPM.name.bold(),
+                         &self.name.clone().unwrap().bold());
+                MPM.error(e.to_string(), ExitStatus::Error);
+            }
+        }
+    }
+
+    fn set_env(&self) {
+        let build = "BUILD";
+        let prefix = "PREFIX";
+        env::set_var(build, "build");
+        env::set_var(prefix, self.prefix.clone().unwrap());
+        match fs::create_dir("build") {
+            Ok(_) => { },
+            Err(e) => {
+                MPM.error(e.to_string(), ExitStatus::Error);
+                panic!();
+            }
+        };
+    }
+
+    fn build(&self) {
+        &self.set_env();
+        for line in self.build.clone().unwrap() {
+            let parsed_line: Vec<&str> = line.split(' ').collect();
+            match parsed_line.split_frst() {
+                Some(s) => {
+                    let command = Command::new(s.0)
+                            .args(s.1)
+                            .output().unwrap_or_else(|e| {
+                                MPM.error(e.to_string(), ExitStatus::Error);
+                                panic!();
+                            });
+                    println!("{}", String::from_utf8_lossy(&command.stdout));
+                },
+                None => { },
+            };
+        }
     }
 }
 
